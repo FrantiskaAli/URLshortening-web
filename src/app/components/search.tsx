b9e8db9d19fdc6ff66a2d377436e783f8bec0fd6
FrantiskaAli <tiskarechk@gmail.com>
2023-06-21 10:53:37 +0100

    sstarted working on mobile view, changed the navbar, jumbo and started with search

--- src/app/components/search.tsx
+++ src/app/components/search.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 
 type SearchedLinks = {
   short: string;
-  original:string
+  original: string
 }[]
 
 export default function Search() {
@@ -16,10 +16,10 @@ export default function Search() {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   }
-  const handleInput = ()=>{
-    if (inputValue.length < 5){
+  const handleInput = () => {
+    if (inputValue.length < 5) {
       setEmpty(true)
-    }else setEmpty(false)
+    } else setEmpty(false)
   }
 
   const handleClick = async () => {
@@ -27,23 +27,23 @@ export default function Search() {
       const response = await fetch(`https://api.shrtco.de/v2/shorten?url=${inputValue}`);
       const shortened = await response.json();
       const final = await shortened.result.full_short_link;
-    
-      setSearched(prev => [...prev,{ original: inputValue, short: final}])// setting original link array
+
+      setSearched(prev => [...prev, { original: inputValue, short: final }])// setting original link array
       console.log(final)
     } catch (error) { console.error('Error:', error); alert("I am sorry, you need to enter valid link") }
   }
 
 
   return (
-    <section className='self-center md:w-4/5 md:-mt-32 '>
-      <article style={{ backgroundImage: "url('bg-shorten-desktop.svg')", backgroundRepeat: "no-repeat", backgroundSize: "cover" }} className='md:py-16 md:h-48 bg-purple-950 self-center flex justify-between rounded-xl md:mt-10 md:px-20'>
-        <section className='flex flex-col w-4/5  '>
-        <input name="link" className={empty ? "h-10 self-center rounded-lg p-6 w-full focus:outline-none ring-2 ring-red-500" : "h-10 w-full self-center rounded-lg p-6 focus:outline-none focus:border-none focus:ring-2 focus:ring-purple-700" } onClick={()=>handleInput()} value={inputValue} onChange={handleChange} type="text" placeholder='Shorten a link here...' />
-        {empty && <label className="text-red-500 font-bold text-sm mt-2">Please add a link</label>}
+    <section className='self-center w-11/12 md:w-4/5 md:-mt-32 -mt-16'>
+      <article style={{ backgroundImage: "url('bg-shorten-desktop.svg')", backgroundRepeat: "no-repeat", backgroundSize: "cover" }} className='md:py-16 md:h-48 bg-purple-950 self-center flex flex-col items-center py-8 px-4 md:flex-row md:justify-between rounded-xl md:mt-10 md:px-20'>
+        <section className='flex flex-col md:w-4/5 w-full relative mb-10 self-center'>
+          <input name="link" className={empty ? "h-10 rounded-md md:rounded-lg p-6 w-full focus:outline-none ring-2 ring-red-500" : "h-10 w-full rounded-md md:rounded-lg  p-6 focus:outline-none focus:border-none focus:ring-2 focus:ring-purple-700"} onClick={() => handleInput()} value={inputValue} onChange={handleChange} type="text" placeholder='Shorten a link here...' />
+          {empty && <label className="text-red-500 font-bold text-sm mt-12 absolute top-2">Please add a link</label>}
         </section>
-        <button onClick={() => handleClick()} className="bg-teal-400 font-bold text-white h-12 px-12 self-center rounded-lg md:-mt-4 hover:bg-teal-200">Shorten It!</button>
+        <button onClick={() => handleClick()} className="bg-teal-400 font-bold text-white h-12 px-12 self-center rounded-lg hover:bg-teal-200 w-full md:w-fit">Shorten It!</button>
       </article>
-      <Saved searched={searched}/>
+      <Saved searched={searched} />
     </section>
   )
 }
